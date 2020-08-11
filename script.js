bed93daf99a58d37fbaf2335ce832ecd8b0d5602
from0toweb <t9052585903@gmail.com>
2020-08-11 15:05:22 +0300

    feat: lesson_04_hard complete

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let showText = function(a) {
+	if (typeof a === 'string') {
+		a = a.trim();
+		if (a.length > 30) {
+			a = a.slice(0, 30) + '...';
+		}
+		return a;
+	} else {
+		return 'в качестве аргумента передана не строка!!!';
+	}
+};
+
+alert(showText(123));
