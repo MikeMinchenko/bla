02540e4527d2bb3eaec7e3ab7e748dccf163658c
from0toweb <t9052585903@gmail.com>
2020-08-15 16:43:25 +0300

    fix: fixed some problems with code in condirions

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 'use strict';
 
-let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let week = [ 'Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' ];
 let date = new Date();
 
 for (let i = 0; i < week.length; i++) {
-  let dayOfWeek = week[i];
-  if (i === date.getDay()) {
-    dayOfWeek = dayOfWeek.bold();
-  } else if (i > 5 || i === 0) {
-    dayOfWeek = dayOfWeek.italics();
-  }
+	let dayOfWeek = week[i];
+	let div = document.createElement('div');
+	div.innerHTML = dayOfWeek;
 
-  let div = document.createElement('div');
-  div.innerHTML = dayOfWeek;
-  document.body.appendChild(div);
+	if ((i === 6 || i === 0) && i === date.getDay()) {
+		div.style.cssText = 'font-style: italic; font-weight: bold';
+	} else if (i === 6 || i === 0) {
+		div.style.cssText = 'font-style: italic';
+	} else if (i === date.getDay()) {
+		div.style.cssText = 'font-weight: bold';
+	}
+
+	document.body.appendChild(div);
 }
