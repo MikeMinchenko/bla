c81666c10991c43146e9b7b83ddb0f9394c83546
from0toweb <t9052585903@gmail.com>
2020-08-24 16:55:34 +0300

    lesson_13_hard complete

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let changeBtn = document.querySelector('#change-btn'),
+  header = document.querySelector('#header');
+
+changeBtn.addEventListener('click', function () {
+  let
+    r = Math.floor(Math.random() * (256)),
+    g = Math.floor(Math.random() * (256)),
+    b = Math.floor(Math.random() * (256)),
+    color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+  console.log(r);
+  console.log(r.toString(16));
+
+
+  document.body.style.background = color;
+  header.textContent = color;
+  changeBtn.style.color = color;
+});
