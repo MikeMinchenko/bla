125daa30a5ed379a47475cdd4fc629152ca73b30
from0toweb <t9052585903@gmail.com>
2020-09-04 14:09:31 +0300

    fix mistakes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 class Todo {
-
 	constructor(form, input, todoList, todoComleted) {
 		this.form = document.querySelector(form);
 		this.input = document.querySelector(input);
@@ -9,23 +8,19 @@ class Todo {
 		this.todoComleted = document.querySelector(todoComleted);
 		this.todoData = new Map(JSON.parse(localStorage.getItem('todoList')));
 		this.todoContainer = document.querySelector('.todo-container');
-
 	}
 
 	addToStorage() {
-		localStorage.setItem('todoList', JSON.stringify([...this.todoData]));
+		localStorage.setItem('todoList', JSON.stringify([ ...this.todoData ]));
 	}
 
 	// возможность редактирования тудушек
 	editing(elem) {
-		const elemKey = elem.getAttribute('key');
-		this.todoData.forEach(item => {
-
-			if (item.key === elemKey) {
+		this.todoData.forEach((item) => {
+			if (item.key === elem.key) {
 				item.value = elem.textContent.trim();
 				item.contenteditable = !item.contenteditable;
 			}
-
 		});
 
 		this.render();
@@ -33,8 +28,7 @@ class Todo {
 
 	// обработчик клика по кнопкам внутри li и по клавише enter
 	handler() {
-
-		this.todoContainer.addEventListener('click', e => {
+		this.todoContainer.addEventListener('click', (e) => {
 			const target = e.target;
 
 			if (target.matches('.todo-remove')) {
@@ -44,10 +38,9 @@ class Todo {
 			} else if (target.matches('.todo-edit')) {
 				this.editing(target.closest('li'));
 			}
-
 		});
 
-		this.todoContainer.addEventListener('keydown', e => {
+		this.todoContainer.addEventListener('keydown', (e) => {
 			if (e.key === 'Enter') {
 				e.preventDefault();
 				this.editing(e.target.closest('li'));
@@ -57,9 +50,8 @@ class Todo {
 
 	// фунция удаления элементов
 	deleteItem(elem) {
-		const elemKey = elem.getAttribute('key');
 		this.todoData.forEach((item, index) => {
-			if (item.key === elemKey) {
+			if (item.key === elem.key) {
 				this.todoData.delete(index);
 			}
 		});
@@ -68,13 +60,10 @@ class Todo {
 
 	// добавляем элемент в список комплит
 	completedItem(elem) {
-		const elemKey = elem.getAttribute('key');
-		this.todoData.forEach(item => {
-
-			if (item.key === elemKey) {
+		this.todoData.forEach((item) => {
+			if (item.key === elem.key) {
 				item.completed = !item.completed;
 			}
-
 		});
 
 		this.render();
@@ -92,26 +81,29 @@ class Todo {
 	createItem(item) {
 		const li = document.createElement('li');
 		li.classList.add('todo-item');
-		li.setAttribute('key', item.key);
+		li.key = item.key;
 		li.setAttribute('contenteditable', item.contenteditable);
-		li.insertAdjacentHTML('beforeend', `
+		li.insertAdjacentHTML(
+			'beforeend',
+			`
 			<span class="text-todo">${item.value}</span>
 			<div class="todo-buttons">
 				<button class="todo-edit"></button>
 				<button class="todo-remove"></button>
 				<button class="todo-complete"></button>
 			</div>
-		`);
+		`
+		);
 
 		if (item.completed) {
 			this.todoComleted.append(li);
 		} else {
 			this.todoList.append(li);
+			console.dir(li);
 		}
 	}
 
 	addTodo(e) {
-
 		e.preventDefault();
 
 		if (this.input.value.trim() !== '') {
@@ -129,14 +121,11 @@ class Todo {
 		}
 
 		this.input.value = '';
-
-
 	}
 
 	generateKey() {
 		return '_' + Math.random().toString(36).substr(2, 9);
 	}
-
 
 	init() {
 		this.form.addEventListener('submit', this.addTodo.bind(this));
@@ -144,7 +133,6 @@ class Todo {
 		this.handler();
 	}
 }
-
 
 const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed');
 
