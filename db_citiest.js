61e09d0569c8ad0bf8beb91858fe719a2104ca02
from0toweb <t9052585903@gmail.com>
2020-09-21 11:26:47 +0300

    lesson_31_3 complete

--- db_citiest.js
+++ db_citiest.js
@@ -1,21 +1,90 @@
 'use strict';
 const loader = document.querySelector('.loader');
 
-setTimeout(() => {
-	fetch('./db_cities.json')
-		.then(response => {
-			if (response.status !== 200) {
-				throw new Error(`Status ${response.status}`);
-			}
+let localLang, dataSave;
 
-			return response.json();
-		})
-		.then(data => {
-			init(data);
+const createLocalLang = () => {
+		let item = prompt('Укажите язык : "RU", "EN", "DE"');
+		localStorage.removeItem('savesCitiesDB');
+
+		if (item !== null) {
+			item = item.toUpperCase().trim();
+			if (item !== 'RU' && item !== 'EN' && item !== 'DE') {
+				createLocalLang();
+			} else {
+				setCookie('lang', item, 2025, 12, 31);
+				saveLang(document.cookie.split('; '));
+			}
+		} else {
+			createLocalLang();
+		}
+	},
+	setCookie = (key, value, year, month, day, path, domain, secure) => {
+		let cookieStr = `${key}=${value}`;
+
+		if (year) {
+			const expires = new Date(year, month, day);
+			cookieStr += `; expires=${expires.toGMTString()}`;
+		}
+
+		cookieStr += path ? `; path=${path}` : '';
+		cookieStr += domain ? `; domain=${domain}` : '';
+		cookieStr += secure ? `; secure` : '';
+
+		document.cookie = cookieStr;
+	},
+	saveLang = arr => {
+		let strCookie = arr.filter(item => item.startsWith('lang=')).join('');
+		localLang = strCookie.split('=')[1];
+
+		dataSave = JSON.parse(localStorage.getItem('savesCitiesDB'));
+
+		if (!dataSave) {
+			fetch('./db_cities.json')
+				.then(response => {
+					if (response.status !== 200) {
+						throw new Error(`Status ${response.status}`);
+					}
+
+					return response.json();
+				})
+				.then(data => {
+					dataSave = data[localLang];
+					sortArr(dataSave);
+					init(dataSave);
+					loader.style.display = 'none';
+					saveLocalStorage();
+				})
+				.catch(error => console.error(error));
+		} else {
 			loader.style.display = 'none';
-		})
-		.catch(error => console.error(error));
-}, 5000);
+			sortArr(dataSave);
+			init(dataSave);
+		}
+	},
+	checkLang = () => {
+		let arr = document.cookie.split('; ');
+		if (arr.some(item => item.startsWith('lang='))) {
+			saveLang(arr);
+		} else {
+			createLocalLang();
+		}
+	},
+	saveLocalStorage = () => {
+		localStorage.setItem('savesCitiesDB', JSON.stringify(dataSave));
+	},
+	sortArr = arr => {
+		if (localLang === 'RU') {
+			let index = dataSave.findIndex(item => item.country === 'Россия');
+			dataSave.unshift(...dataSave.splice(index, 1));
+		} else if (localLang === 'EN') {
+			let index = dataSave.findIndex(item => item.country === 'United Kingdom');
+			dataSave.unshift(...dataSave.splice(index, 1));
+		} else if (localLang === 'DE') {
+			let index = dataSave.findIndex(item => item.country === 'Deutschland');
+			dataSave.unshift(...dataSave.splice(index, 1));
+		}
+	};
 
 const init = data => {
 	// Переменные
@@ -35,7 +104,7 @@ const init = data => {
 	const renderListDefault = () => {
 			const block = clearBlock(listDefault);
 
-			data['RU'].forEach(item => {
+			data.forEach(item => {
 				const countryBlock = createCounrtyBlock();
 				item.cities.sort((a, b) => {
 					return b.count - a.count;
@@ -72,7 +141,7 @@ const init = data => {
 		renderListSelect = country => {
 			const block = clearBlock(listSelect);
 
-			data['RU'].forEach(item => {
+			data.forEach(item => {
 				if (item.country === country) {
 					const countryBlock = createCounrtyBlock();
 
@@ -108,7 +177,7 @@ const init = data => {
 			const block = clearBlock(listAutocomplete);
 			const countryBlock = createCounrtyBlock();
 			block.append(countryBlock);
-			data['RU'].forEach(item => {
+			data.forEach(item => {
 				item.cities.forEach(city => {
 					const fixItem = city.name.toLowerCase();
 
@@ -282,3 +351,5 @@ const init = data => {
 
 	closeButton.addEventListener('click', resetSelect);
 };
+
+checkLang();
