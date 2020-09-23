f985d0f9a821a3dee30e09437aba3b27a46add2c
from0toweb <t9052585903@gmail.com>
2020-09-23 11:59:46 +0300

    feat: add new popups to toggleMenu

--- src/modules/togglePopups.js
+++ src/modules/togglePopups.js
@@ -1,5 +1,9 @@
 const togglePopups = () => {
-	const popupMenu = document.querySelector('.popup-menu');
+	const popupMenu = document.querySelector('.popup-menu'),
+		popupRepairTypes = document.querySelector('.popup-repair-types'),
+		popupPrivacy = document.querySelector('.popup-privacy'),
+		popupConsultation = document.querySelector('.popup-consultation'),
+		popupDesign = document.querySelector('.popup-design');
 
 	document.addEventListener('click', event => {
 		const target = event.target;
@@ -11,6 +15,12 @@ const togglePopups = () => {
 			popupMenu.querySelector('.popup-dialog-menu').classList.remove('showHide-menu');
 			popupMenu.style.visibility = `hidden`;
 		}
+
+		if (target.matches('.link-list, .link-list a')) popupRepairTypes.style.visibility = `visible`;
+		if (target.matches('.link-privacy')) popupPrivacy.style.visibility = `visible`;
+		if (target.matches('.button_wide')) popupConsultation.style.visibility = `visible`;
+		if (target.matches('.link-list-designs a')) popupDesign.style.visibility = `visible`;
+		if (target.matches('.popup') || target.matches('.close')) target.closest('.popup').style.visibility = `hidden`;
 	});
 };
 
