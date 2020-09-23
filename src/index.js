a7cae48f0a2517f8889c886267861f0936da6c54
from0toweb <t9052585903@gmail.com>
2020-09-23 12:32:39 +0300

    feat: add maskPhone

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import togglePhone from './modules/togglePhone';
-import togglePopups from './modules/togglePopups';
+import togglePhone from './modules/togglePhone.js';
+import togglePopups from './modules/togglePopups.js';
+import smoothScroll from './modules/smoothScroll.js';
+import maskPhone from './modules/maskPhone.js';
 
 togglePhone();
 togglePopups();
+smoothScroll();
+maskPhone('[name="phone"]');
---
+++ src/modules/smoothScroll.js
@@ -0,0 +1,21 @@
+const smoothScroll = () => {
+	const anchors = document.querySelectorAll('a[href*="#"]');
+
+	for (const anchor of anchors) {
+		anchor.addEventListener('click', event => {
+			event.preventDefault();
+
+			const blockID = anchor.getAttribute('href'),
+				idElem = document.querySelector(blockID);
+			if (idElem) {
+				const idElemY = idElem.offsetTop;
+				window.scrollTo({
+					top: idElemY,
+					behavior: 'smooth'
+				});
+			}
+		});
+	}
+};
+
+export default smoothScroll;
